b47ed95aa7364c78d9f5a6db27673910f4e995c9
Savurdu <81192424+Savurdu@users.noreply.github.com>
2021-06-24 00:43:59 +0300

    Create termux.sh

---
+++ termux.sh
@@ -0,0 +1,8 @@
+echo "Gerekli moduller yukleniyor"
+pkg install python
+pkg install python3
+pkg install python3-pip
+pip install requests
+pip install colorama
+echo "SavurduBomb Hazır."
+python3 bomber.py
