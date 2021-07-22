3d5939a07fba952fcc3a886edb017fa921642ee1
Savurdu <81192424+Savurdu@users.noreply.github.com>
2021-07-22 15:59:15 +0300

    Delete termux.sh

--- termux.sh
+++
@@ -1,8 +0,0 @@
-echo "Gerekli moduller yukleniyor"
-pkg install python
-pkg install python3
-pkg install python3-pip
-pip install requests
-pip install colorama
-echo "SavurduBomb Hazir."
-python3 bomber.py
